f452d9e6be98b6134857a023b1a5284aed8e6227
sayon-bitquery <85619516+sayon-bitquery@users.noreply.github.com>
2021-07-28 13:39:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,33 +6,35 @@ app.set('view engine', 'ejs')
 const endpoint = "https://graphql.bitquery.io/" 
 const query = `
 {
-	bitcoin{
-    	blocks{
-      		count
-    	}
+	bitcoin {
+    		blocks {
+      			count
+    		}
    	}
 }`
 
+let countValue = 0; 
+
 function BitqueryAPICall(){
-	const result =  fetch(endpoint, {
+	const data =  fetch(endpoint, {
 	    method: "POST",
 	    headers: {
 	    	"Content-Type": "application/json",
-	      	"X-API-KEY": "YOUR UNIQUE API KEY"
+	      	"X-API-KEY": "BQYUGuoO6tZKM20I0lfBNCTEC4ouBCT1"
 	    },
 	    body: JSON.stringify({
 	        query
 	    })
 	}).then(res => res.json()).then(data => {
 		countValue = data.data.bitcoin.blocks[0].count
+		console.log(countValue)
 	})
-	return result
+
 }
 
-let countValue = BitqueryAPICall()
 
 app.get('/', (req, res)=>{
-	console.log(countValue+" is the returned value!")
+	console.log(BitqueryAPICall())
 	res.render('home', {result: countValue})
 })
 
